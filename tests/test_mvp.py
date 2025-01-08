997e803da565045b825c74a05871be99c0d063a7
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-01-07 20:05:13 -0800

    Added parametrized pytest test cases

--- tests/test_mvp.py
+++ tests/test_mvp.py
@@ -1,5 +1,6 @@
 from src.mvp import *
 import pytest
 
-def test_functionality():
-    assert type(game('R')) == str
+@pytest.mark.parametrize("test_input,expected", [("R", str), ("P", str), ("S", str)])
+def test_functionality(test_input, expected):
+    assert type(game(test_input)) == expected
